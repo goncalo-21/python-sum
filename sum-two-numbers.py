fbfc69b36a97ce92f74e7519d98e55b22fcdf810
Goncalo Dourado <goncalo.peixotodourado@tno.nl>
2025-02-04 10:14:42 +0100

    Add docstring

--- sum-two-numbers.py
+++ sum-two-numbers.py
@@ -1,2 +1,12 @@
 def sum_two_numbers(a,b):
+    """
+    Sums two numbers and returns the result.
+    
+    Parameters:
+    a (int): the first number
+    b (int): the second number
+    
+    Return:
+    int: The sum of the two numbers
+    """
     return a + b
